0615d2bfcb6fdd73955880ae81497b41198b5e9c
Evan Miller <ewmst5@gmail.com>
2025-05-19 00:08:03 -0400

    Ok. I am going to have to figure out some details tomorrow.  I was able to get it working at various points, and then I somehow broke it.

--- script.js
+++ script.js
@@ -1,55 +1,39 @@
-let taskInput = document.getElementById("taskInput");
-let addTaskButton = document.getElementById("addTaskButton");
-let taskList = document.getElementById("taskList");
+const inputField = document.getElementById("input-field");
+const listContainer = document.getElementById("list-container");
 
-// console.log(taskInput);
-// console.log(addTaskButton);
-// console.log(taskList);
 
-// Adds an event listener to the button
-addTaskButton.addEventListener("click", function () {
-  // store the value input value in a variable
-  let taskText = taskInput.value;
 
-  // check if the input text is empty
-  if (taskText === "") {
-    alert("Please enter a task!");
-    return;
+function addTask(){
+  if (inputField.value === '') {
+    alert("Don't forget to add a task.");
   }
-
-  // create a new li and add the user text to the li
-  let listItem = document.createElement("li");
-  listItem.innerText = taskText;
-
-  // Create a new checkbox
-  let checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-
-  // Append checkbox to the li
-  listItem.appendChild(checkbox);
-
-  // add the li to the ul
-  taskList.appendChild(listItem);
-  taskInput.value = ""; // Clear the input field
-});
-
-// Remove an li from the ul
-taskList.addEventListener("click", function (event) {
-  console.log(event);
-  // checks if the click was on an li
-  if (event.target.tagName === "LI") {
-    //remove the element child from the ul
-    taskList.removeChild(event.target);
+  else{
+    let li = document.createElement("li");
+    li.innerHTML = inputField.value;
+    listContainer.appendChild(li);
+    let span = document.creatElement("span");
+      span.innerHTML = "\u00d7";
+      li.appendChild(span);
   }
-
-  if (event.target.tagName === "INPUT") {
-    //remove the element child from the ul
-    console.log(event.target.parentNode);
-    
-    event.target.parentNode.classList.toggle("completed");
-  }
-});
-
-
-
   
+  inputField.value = "";
+  saveInfo();
+}
+  
+listContainer.addEventListener("click", function(event){
+    if(event.target.tagName === "LI"){
+      event.target.classlist.toggle("checked");
+    } else if(event.target.name === "SPAN"){
+      event.target.parentElement.remove();
+      saveInfo();
+    }
+  }, false);
+
+  function saveInfo(){
+    localStorage.setItem("info", listContainer.innerHTML);
+  }
+
+  function showTask(){
+    listContainer.innerHTML = localStorage.getItem("info");
+  }
+  showTask();
